12436b244058a7e0bf6cd5d5bc84cc0017c75cd7
ReinhardSchmidt <78225531+ReinhardSchmidt@users.noreply.github.com>
2021-10-23 18:27:46 +0200

    Update home.tsx

--- service/constants/home.tsx
+++ service/constants/home.tsx
@@ -216,18 +216,13 @@ export const dataPeopleBuildingHopr = [
 
 export const paragraphHopr = (
   <>
-    At HOPR, we take cyber security seriously. HOPR is an IT consultancy with
-    its own in-house development team. <br />
-    If you need to secure your company’s data, or the data you receive from your
+    At HOPR, we take cyber security seriously. HOPR is an IT consultancy with its own in-house development team. If you need to secure your company’s data, or the data you receive from your
     clients, HOPR has you covered.
     <br />
     What makes us different?
-    <br />
     There are many data storage solutions, either on-site or in the cloud. But
     the real problems arise when data is transferred. HOPR ensures that data can
-    be sent and received safely, easily and with no risk of data leaks.
-    <br />
-    We consult with companies and institutions and provide solutions for all
+    be sent and received safely, easily and with no risk of data leaks. We consult with companies and institutions and provide solutions for all
     their data problems.
   </>
 );
@@ -268,13 +263,15 @@ export const hoprTeam = (
 
 export const addres1 = (
   <>
+    Europe:
+    <br />
     HOPR Services AG
     <br />
     Bleicherweg 33
     <br />
     8002 Zurich
     <br />
-    Switzerlad
+    Switzerland
   </>
 );
 
